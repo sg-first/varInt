988f76b18664c6170c0e746ec2d2b0a5b21fa75d
3A <524019351@qq.com>
2021-07-02 20:01:53 +0800

    Bit operation implementation(more efficient)

--- test.cpp
+++ test.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main()
 {
-    for (UInt8 i = 0;i < 128;i++)
+    for (UInt64 i = 0;i < 1000000;i++)
     {
         VarInt v(i);
-        if (v.AsUInt64() != i)
+        auto r = v.AsUInt64();
+        if (r != i)
             cout << "fuck" << endl;
     }
-    for (SInt64 i = -10000;i < 1000;i++)
+    for (SInt64 i = -10000;i < 10000;i++)
     {
         VarInt v(i);
         SInt64 r = v.AsSInt64();
--- varint.h
+++ varint.h
@@ -15,68 +15,61 @@ using UInt64 = std::uint64_t;
 class VarInt
 {
 private:
-    void push(const std::bitset<8>& nowBinElm) { this->bin.push_back(UInt8(nowBinElm.to_ulong())); }
+    std::vector<UInt8> bin;
 
-    template<typename T, UInt8 LEN>
-    void init(T i)
+    void init(UInt64 i)
     {
-        std::bitset<LEN> bi(i);
+        if (i == 0)
+            return;
+
+        UInt8* ptr = (UInt8*)&i;
+
+        std::bitset<64> bi(i);
         //find first 1 from the highest bit(real highest bit)
-        short stopPos = LEN - 1;
+        short stopPos = 63;
         for (;stopPos >= 0;stopPos--)
         {
             if (bi.test(stopPos))
                 break;
         }
 
-        bool firstEnter = true;
-        std::bitset<8> nowBinElm; //bin中的元素
-        short nowPos = 0; //为了标识结束（最后1截止），实际数据是反着存的。前面的是高位（反应在bitset里，下标小的是高位）
-        do
+        auto isEnd = [&ptr, &stopPos](short nextHightestBit) { return nextHightestBit>=stopPos && ((ptr[0] & 0b10000000) == 0); }; //这个不对，还得找最高位位置
+
+        short nextHightestBit = 7; //下一次要处理部分的最高位（下标）
+        while (true)
         {
-            //push to bin
-            if (firstEnter)
+            UInt8 temp = ptr[0];
+            if (isEnd(nextHightestBit)) //看到没到最后一位（如果当前位首位是1，还需要再处理一次。因此条件是检测首位为0）
             {
-                firstEnter = false;
+                //满足第二个条件首位一定是0，所以不用设flag bit
+                bin.push_back(temp); //最高位放在最后
+                break;
             }
             else
             {
-                nowBinElm.set(8 - 1); //flag bit set to 1（在这里push一定后面还有，因为即使nowPos是最后一位，这个最后一位也要在下面处理）
-                this->push(nowBinElm);
-                nowBinElm.reset();
+                temp |= 0b10000000;
+                this->bin.insert(this->bin.begin(), temp); //越高位下标越小
+                i >>= 7; //把已处理完的7位去掉
+                nextHightestBit += 7;
             }
-
-            //move bits to nowBinElm（nowBinElm的最高位是标志位，不在这个循环里写）
-            for (short i = 6;i >= 0 && nowPos <= stopPos;i--, nowPos++)
-            {
-                //将bi中的位写入该元素（i从高位到低位，nowPos从低位到高位）
-                nowBinElm[i] = bi[nowPos];
-            }
-        } while (nowPos <= stopPos);
-
-        if (nowBinElm.any()) //nowBinElm不全为0（因为反着存，最后一位一定是1），说明还得push一下
-            this->push(nowBinElm);
+        }
     }
 
 public:
-    std::vector<UInt8> bin;
-
-    VarInt(UInt8 i) { this->init<UInt8, 8>(i); }
-    VarInt(UInt16 i) { this->init<UInt16, 16>(i); }
-    VarInt(UInt32 i) { this->init<UInt32, 32>(i); }
-    VarInt(UInt64 i) { this->init<UInt64, 64>(i); }
+    VarInt(UInt64 i) { this->init(i); }
     VarInt(SInt64 i)
     {
         UInt64 zipcode = abs(i) * 2;
         if (i > 0)
             zipcode--; //正数在前
-        this->init<UInt64, 64>(zipcode);
+        this->init(zipcode);
     }
 
+    std::vector<UInt8> getBin() { return this->bin; }
     SInt64 AsSInt64() const { return AsSInt64(this->bin); }
-    static SInt64 AsSInt64(const std::vector<UInt8>& bin)
+    static SInt64 AsSInt64(const std::vector<UInt8>& bin, size_t startPos = 0, size_t endPos = 0)
     {
-        UInt64 zigcode = AsUInt64(bin);
+        UInt64 zigcode = AsUInt64(bin, startPos, endPos);
         SInt64 result = 0;
         if (zigcode % 2 == 0)
             result = -SInt64(zigcode) / 2; //偶数下标是负数
@@ -86,36 +79,51 @@ public:
     }
 
     UInt64 AsUInt64() const { return AsUInt64(this->bin); }
-    static UInt64 AsUInt64(const std::vector<UInt8>& bin)
+    static UInt64 AsUInt64(const std::vector<UInt8>& bin, size_t startPos = 0, size_t endPos = 0)
     {
-        std::bitset<64> result;
-        short nowPos = 0;
-        for (const UInt8& i : bin)
+        if (bin.empty())
+            return 0;
+        else if (bin.size() == 1)
+            return bin[0];
+        else
         {
-            std::bitset<8> bi(i);
-            if (bi.test(8 - 1)) //not last block
-            {
-                for (short i = 6;i >= 0;i--, nowPos++)
-                {
-                    result[nowPos] = bi[i];
-                }
-            }
-            else
-            {
-                //从低位（下标小）开始查一下最后一个1的位置
-                short stopI = 0;
-                for (;stopI <= 6;stopI++)
-                {
-                    if (bi.test(stopI))
-                        break;
-                }
+            if (endPos == 0)
+                endPos = bin.size();
 
-                for (short i = 6;i >= stopI;i--, nowPos++)
+            UInt64 result = 0;
+            UInt8* ptr = (UInt8*)&result;
+            short nowSub = 0; //小的是低字节
+
+            auto isLast = [](const UInt8& i) { return (i & 0b10000000) == 0; };
+
+            for (size_t i = startPos;i < endPos;i++)
+            {
+                UInt8 ri = bin[i];
+                if (isLast(ri))
                 {
-                    result[nowPos] = bi[i];
+                    if (nowSub >= 2) //消除左边多出来的0
+                    {
+                        if ((ri & 0b00000001) == 1) //取最高字节中的最低位，并将ptr[nowSub]最高位与其设为一致
+                            ptr[nowSub - 1] |= 0b10000000;
+                        ri >>= 1; //最高字节中最低位已被移走
+                        
+                    }
+                    ptr[nowSub] = ri; //最后一字节放在最高字节 
+                    result >>= 1; //消除else中标志位造成的误差（右边多出来的0）
+                    break;
+                }
+                else
+                {
+                    result <<= 7; //腾地方（nowSub=1后再进行，左边会有一个多出来的0）
+                    //把新的放进来
+                    ri <<= 1; //remove flag bit（这步结束之后右边有个多出来的0）
+                    ptr[0] = ri; //现在的放到最低字节（下标越大的是越低位）
+                    
+                    nowSub++; //现在的最高字节到哪了
                 }
             }
+            
+            return result;
         }
-        return result.to_ullong();
     }
 };
